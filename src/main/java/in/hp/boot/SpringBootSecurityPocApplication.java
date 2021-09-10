44e27ed83897afc991550adcd46ecd10260c53c1
Hariprasath R <balaprasad24@gmail.com>
2021-09-10 21:03:41 +0530

    Upgraded Spring and Java Version

--- src/main/java/in/hp/boot/SpringBootSecurityPocApplication.java
+++ src/main/java/in/hp/boot/SpringBootSecurityPocApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSecurityPocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityPocApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityPocApplication.class, args);
+    }
 
 }
